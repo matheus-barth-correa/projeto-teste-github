c2515dd610cd1f65ad81af51158b3ddd7ae3740f
matheus-barth-correa <106935169+matheus-barth-correa@users.noreply.github.com>
2022-10-16 20:29:43 -0300

    m

---
+++ script.js
@@ -0,0 +1,14 @@
+function somar() {
+    var n1=document.getElementById('one')
+    var n2=document.getElementById('two')
+    var number1=Number(n1.value)
+    var number2=Number(n2.value)
+    var resultado=number1+number2
+}
+res.innerHTML(`${resultado}`)
+function number1() {
+    n1=1
+}
+function number2() {
+    n2=2
+}
